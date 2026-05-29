c3f6a6c04611dc69f1f6e2f3ab471507e904cb9c
Velez Gae <20253ds067@utez.edu.mx>
2026-05-29 10:16:00 -0600

    tercer push

---
+++ js/carrusel2.js
@@ -0,0 +1,14 @@
+
+
+
+let indice =0;
+
+let images = ["img/1.jpg", "img/2.jpg"];
+
+
+document.getElementById("siguiente").addEventListener("click",()=>{
+    if (indice < 5) {
+        indice++;
+    }
+    document.getElementById("img").setAttribute("src",images[indice]);
+});
