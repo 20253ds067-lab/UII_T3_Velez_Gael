b0b5c190f722212a668650bb0107eae7e9e1c9e1
Velez Gae <20253ds067@utez.edu.mx>
2026-05-29 10:23:13 -0600

    cuarto push

--- js/carrusel2.js
+++ js/carrusel2.js
@@ -3,7 +3,7 @@
 
 let indice =0;
 
-let images = ["img/1.jpg", "img/2.jpg"];
+let images = ["img/1.jpg", "img/2.jpg,img/3.jpg", "img/4.jpg", "img/5.jpg"];
 
 
 document.getElementById("siguiente").addEventListener("click",()=>{
