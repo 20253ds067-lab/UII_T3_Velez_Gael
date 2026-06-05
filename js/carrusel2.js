b4c9d346a0c5e0d60ad33bdc2bd51d05a1984bb1
ixaya <20253ds067@utez.edu.mx>
2026-06-05 00:29:06 -0600

    carrusl2

--- js/carrusel2.js
+++ js/carrusel2.js
@@ -7,8 +7,15 @@ let images = ["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg"];
 
 
 document.getElementById("siguiente").addEventListener("click",()=>{
-    if (indice < 5) {
+    if (indice < images.length - 1) {
         indice++;
     }
     document.getElementById("img").setAttribute("src",images[indice]);
+});
+
+document.getElementById("anterior").addEventListener("click", () => {
+    if (indice > 0) {
+        indice--;
+        document.getElementById("img").setAttribute("src", images[indice]);
+    }
 });
